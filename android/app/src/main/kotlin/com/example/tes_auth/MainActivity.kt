7322befa1ebbbc1d8c232e91a3edb262dfdd7fae
penhele <stephenhelk@gmail.com>
2025-02-26 14:32:41 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/tes_auth/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tes_auth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import '../db/auth_repository.dart';
+import '../provider/auth_provider.dart';
+import '../routes/router_delegate.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'common/url_strategy.dart';
+import 'routes/route_information_parser.dart';
+
+void main() {
+  usePathUrlStrategy();
+  runApp(const QuoteApp());
+}
+
+class QuoteApp extends StatefulWidget {
+  const QuoteApp({super.key});
+
+  @override
+  State<QuoteApp> createState() => _QuoteAppState();
+}
+
+class _QuoteAppState extends State<QuoteApp> {
+  late MyRouterDelegate myRouterDelegate;
+  late MyRouteInformationParser myRouteInformationParser;
+  late AuthProvider authProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    final authRepository = AuthRepository();
+
+    authProvider = AuthProvider(authRepository);
+
+    myRouterDelegate = MyRouterDelegate(authRepository);
+
+    myRouteInformationParser = MyRouteInformationParser();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => authProvider,
+      child: MaterialApp.router(
+        title: 'Quotes App',
+        routerDelegate: myRouterDelegate,
+        routeInformationParser: myRouteInformationParser,
+        backButtonDispatcher: RootBackButtonDispatcher(),
+      ),
+    );
+  }
+}
